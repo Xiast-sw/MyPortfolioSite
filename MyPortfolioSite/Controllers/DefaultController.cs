4bb0db464cb4b1b9848362aa4555912e1930af0b
Xiast-sw <a.bugraaytar@gmail.com>
2024-11-14 23:37:51 +0300

    Master geçmişi üzerinden new-branch değişiklikleri eklendi

---
+++ MyPortfolioSite/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioSite.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolioSite/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioSite.DAL.Entities;
+
+namespace MyPortfolioSite.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public MyPortfolioContext(DbContextOptions<MyPortfolioContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
