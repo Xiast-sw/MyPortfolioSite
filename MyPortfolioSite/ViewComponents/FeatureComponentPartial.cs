4485639100e868f592c12bbd0ba2f6c6294c525c
Xiast-sw <a.bugraaytar@gmail.com>
2024-11-14 22:56:04 +0300

    guncelleme

---
+++ MyPortfolioSite/ViewComponents/FeatureComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioSite.ViewComponents
+{
+    public class FeatureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
---
+++ MyPortfolioSite/ViewComponents/NavbarComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioSite.ViewComponents
+{
+    public class NavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
